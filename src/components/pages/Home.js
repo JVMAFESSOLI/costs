9050139448f594d6d06cd0f7147f44b40ee642fe
JOSE VITOR MAFESSOLI <jvmafessoli.10@gmail.com>
2022-04-15 21:40:47 -0300

    Criação banco de dados json

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,6 @@
 import styles from './Home.module.css';
 import savings from '../../img/savings.svg';
 import LinkButton from '../layout/LinkButton';
-import { Link } from 'react-router-dom';
 
 function Home(){
     return (
