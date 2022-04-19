b297fc90ba5dc1dbd23d284caf70e20aedb84eb4
JOSE VITOR MAFESSOLI <jvmafessoli.10@gmail.com>
2022-04-19 19:38:44 -0300

    Removendo projetos, resgatando e criando loader

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,7 +1,16 @@
 import {useLocation} from 'react-router-dom'
+import {useState, useEffect} from 'react'
 import Message from '../layout/Message.js'
+import Container from '../layout/Container.js'
+import styles from './Projects.module.css'
+import LinkButton from '../layout/LinkButton.js'
+import ProjectCard from '../project/ProjectCard.js'
+import Loading from '../layout/Loading.js'
 
 function Projects(){
+    const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
+    const [projectMessage, setProjectMessage] = useState('')
 
     const location = useLocation()
     let message = ''
@@ -9,11 +18,63 @@ function Projects(){
         message = location.state.message
     }
 
+    useEffect(() => {
+        setTimeout(() => {
+            fetch('http://localhost:5000/projects', {
+            method:'GET',
+            headers: {
+                'Content-Type':'application/json',
+            },
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                console.log(data)
+                setProjects(data)
+                setRemoveLoading(true)
+            })
+            .catch((err) => console.log(err))}, 300)
+        
+        }, [])
+
+        function removeProject(id){
+            fetch(`http://localhost:5000/projects/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            }).then(resp => resp.json())
+            .then(() => {
+                setProjects(projects.filter((project) => project.id !== id))
+                setProjectMessage('Projeto removido com sucesso!')
+            })
+            .catch(err => console.log(err))
+            
+        }
 
     return (
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/NewProject.js" text="Criar Projeto"/>
+                </div>
             {message && <Message type="success" msg={message} />}
+            {projectMessage && <Message type="success" msg={projectMessage} />}
+            <Container customClass="start">
+                {projects.length > 0 &&
+                projects.map((project) => <ProjectCard
+                    id={project.id}
+                    name={project.name} 
+                    budget={project.budget}
+                    category={project.category.name}
+                    key={project.id}
+                    handleRemove={removeProject}
+                 />)}
+                 {!removeLoading && <Loading />}
+                 {removeLoading && projects.length === 0 && (
+                     <p>Não há projetos cadastrados!</p>
+                 )
+                 }
+            </Container>
         </div>
     )
 }
