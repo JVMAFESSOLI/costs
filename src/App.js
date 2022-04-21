7147c0baf4b6761e9f1a29afbdc2893942c9f730
JOSE VITOR MAFESSOLI <jvmafessoli.10@gmail.com>
2022-04-21 18:10:35 -0300

    Finalização do projeto

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Contact from './components/pages/Contact.js';
 import Company from './components/pages/Company.js';
 import NewProject from './components/pages/NewProject.js';
 import Projects from './components/pages/Projects.js';
+import Project from './components/pages/Project.js';
 
 import Container from './components/layout/Container.js';
 import NavBar from './components/layout/NavBar.js';
@@ -20,6 +21,7 @@ function App() {
             <Route path="/Contact.js" element={<Contact />} />
             <Route path="/Company.js" element={<Company />} />
             <Route path="/NewProject.js" element={<NewProject />} />
+            <Route path="/project/:id" element={<Project />} />
           </Routes>
         </Container>
       <Footer />
